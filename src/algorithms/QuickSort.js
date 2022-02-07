ab2c24b1bcdf27d5b9a2f3e26e7a4aef7a84dc2c
xsuran07 <xsuran07@stud.fit.vutbr.cz>
2022-02-07 13:16:25 +0100

    Fix animatin in Quick sort

--- src/algorithms/QuickSort.js
+++ src/algorithms/QuickSort.js
@@ -4,6 +4,7 @@ export default class QuickSort extends BaseAlgorithm {
     init(values, setValues, setItemsColor) {
         super.init(values, setValues, setItemsColor);
 
+        this.pivotSwap = false;
         this.stack = [0, this.values.length - 1];
         this.top = 1;
         this.newIter = true;
@@ -11,7 +12,32 @@ export default class QuickSort extends BaseAlgorithm {
     }
 
     animate() {
-        this.swap(this.i, this.oldJ, false);
+        if(this.pivotSwap) {
+            this.pivotSwap = false;
+
+            this.finalChange = true;
+            this.swap(this.h, this.i + 1);
+
+            this.newIter = true;
+
+            let p = this.i + 1;
+
+            if(p - 1 > this.l) {
+                this.stack[++this.top] = this.l;
+                this.stack[++this.top] = p - 1;
+            } else if(p - 1 === this.l) {
+                this.sorted.push(this.getIndex(p - 1));
+            }
+
+            if(p + 1 < this.h) {
+                this.stack[++this.top] = p + 1;
+                this.stack[++this.top] = this.h;
+            } else if(p + 1 === this.h) {
+                this.sorted.push(this.getIndex(p + 1));
+            }
+        } else {
+            this.swap(this.i, this.oldJ, false);
+        }
     }
 
     resetValues() {
@@ -35,25 +61,7 @@ export default class QuickSort extends BaseAlgorithm {
             this.resetValues();
         }
 
-        if(!this.partition()) {
-            this.newIter = true;
-
-            let p = this.i + 1;
-
-            if(p - 1 > this.l) {
-                this.stack[++this.top] = this.l;
-                this.stack[++this.top] = p - 1;
-            } else if(p - 1 === this.l) {
-                this.sorted.push(this.getIndex(p - 1));
-            }
-
-            if(p + 1 < this.h) {
-                this.stack[++this.top] = p + 1;
-                this.stack[++this.top] = this.h;
-            } else if(p + 1 === this.h) {
-                this.sorted.push(this.getIndex(p + 1));
-            }
-        }
+        this.partition()
     }
 
     testEnd() {
@@ -78,11 +86,9 @@ export default class QuickSort extends BaseAlgorithm {
             this.j++;
             return true;
         } else {
-            this.finalChange = true;
-            this.swap(this.h, this.i + 1);
-
+            this.pivotSwap = true;
+            this.animation = true;
             return false;
         }
     }
-
 }
